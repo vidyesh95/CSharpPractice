5ae6d3a08da6a6cc949821f3f8db0ea97c379af5
vinit-churi <vinitchuri@hotmail.com>
2025-01-22 17:13:22 +0530

    LINQ

--- CSharpPractice/Program.cs
+++ CSharpPractice/Program.cs
@@ -536,7 +536,8 @@ class Program
  * LINQ (Language Integrated Query) and IEnumerable
  */
 // Specify the data source.
-int[] scores = [97, 92, 81, 60];
+// int[] scores = [97, 92, 81, 60];
+List<int> scores = [97, 92, 81, 60];
 
 // Define the query expression.
 IEnumerable<int> scoreQuery =
@@ -549,6 +550,29 @@ foreach (var i in scoreQuery)
 {
     Console.Write(i + " ");
 } // Output: 97 92 81
+
+
+
+
+/*
+ * LINQ (Language Integrated Query) and IEnumerable
+ */
+
+// The Three Parts of a LINQ Query:
+// 1. Data source.
+int[] numbers = [ 0, 1, 2, 3, 4, 5, 6 ];
+
+// 2. Query creation.
+// numQuery is an IEnumerable<int>
+var numQuery = from num in numbers
+    where (num % 2) == 0
+    select num;
+
+// 3. Query execution.
+foreach (int num in numQuery)
+{
+    Console.Write("{0,1} ", num);
+}
 
 
  
